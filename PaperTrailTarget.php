f66de45b155656fe071a3d3180e24b36ef46db4f
Ezekiel Fernandez <ezekiel_p_fernandez@yahoo.com>
2015-03-03 17:29:23 +0800

    Initial commit

---
+++ PaperTrailTarget.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @link https://github.com/fernandezekiel/yii2-papertrail
+ * @copyright Copyright (c) 2015 Ezekiel Fernandez
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace common\fernandezekiel\papertrail;
+
+
+use yii\helpers\VarDumper;
+use yii\log\Target;
+
+/**
+ * Log target class for logging message to papertrail server
+ */
+class PaperTrailTarget extends Target
+{
+    /**
+     * @var string url of the papertrail server
+     */
+    public $url = 'logs.papertrailapp.com';
+
+    /**
+     * @var int server port of the papertrail server
+     */
+    public $port;
+
+    public function export()
+    {
+        foreach ($this->messages as $message) {
+            list($text, $level, $category, $timestamp) = $message;
+            if (!is_string($text)) {
+                $text = VarDumper::export($text);
+            }
+
+            $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+            socket_sendto($sock, $text, strlen($text), 0, $this->url, $this->port);
+            socket_close($sock);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Yii2 PaperTrail logging
+=========
